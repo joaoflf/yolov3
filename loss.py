7124b932d4940d70e0c14af6b239438c45548884
joaoflf <fernandesj@protonmail.com>
2022-08-04 11:47:11 +0100

    Add obj loss

--- loss.py
+++ loss.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.ops import box_iou
+
 import config
 from dataset import YoloVOCDataset
 from model import YoloV3
@@ -29,23 +30,13 @@ class YoloV3Loss(nn.Module):
         no_obj_loss = self.bce(predictions[..., 0:1][no_obj], targets[..., 0:1][no_obj])
 
         anchors = anchors[None, :, None, None, :]  # reshape anchors perform operationsj
+        b_w_h = torch.exp(targets[..., 3:5]) * anchors  # as in paper
 
-        b_w_h = torch.exp(predictions[..., 3:5]) * anchors  # as in paper
-        print(predictions[..., 3:5].shape)
         # get the indices of the boxes with objects to find the cell numbers
-        object_boxes = obj.nonzero().squeeze().tolist()
-        c_x_y = torch.tensor([box[2:4] for box in object_boxes])
-        print(c_x_y.shape)
-        c_x_y = c_x_y[None, None, None, :]
-        print(self.sigmoid(predictions[..., 1:3]).shape, c_x_y.shape)
-        b_x_y = self.sigmoid(predictions[..., 1:3]) + c_x_y
-        # get the indices of the boxes with objects to find the cell numbers
-        # object_boxes = obj.nonzero().squeeze().tolist()
-        # anchors_with_origin = torch.tensor(
-        #     [[0.5, 0.5] + list(anchors[object_box[1]]) for object_box in object_boxes]
-        # )
-        # print(targets[..., 1:5][obj], anchors_with_origin)
-        # print(box_iou(targets[..., 1:5][obj], anchors_with_origin))
+        object_cells = obj.nonzero().squeeze()[..., 2:4]
+        b_x_y = self.sigmoid(targets[..., 1:3][obj]) + object_cells
+        iou = box_iou(torch.cat([b_x_y, b_w_h[obj]], dim=1), targets[..., 1:5][obj])
+        obj_loss = self.bce(torch.diag(iou), predictions[..., 0:1][obj].squeeze())
         return predictions
 
 

4492c06c28a4e47201146748c51d054c437ca59c
joaoflf <fernandesj@protonmail.com>
2022-09-02 14:46:22 +0100

    Overfitting ok but nms not working

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Trainer:
         model: nn.Module,
         dataloader: DataLoader,
         loss_fn: nn.Module,
+        scaled_anchors: torch.Tensor,
         optimizer: Callable,
         epochs: int,
         learning_rate: float,
@@ -29,6 +30,7 @@ class Trainer:
         self.model = model
         self.dataloader = dataloader
         self.loss_fn = loss_fn
+        self.scaled_anchors = scaled_anchors
         self.optimizer = optimizer(model.parameters(), learning_rate)
         self.epochs = epochs
         self.learning_rate = learning_rate
@@ -57,17 +59,17 @@ class Trainer:
             self.loss_fn(
                 outputs[0].to(config.DEVICE),
                 labels[0].to(config.DEVICE),
-                torch.tensor(config.ANCHORS[0]).to(config.DEVICE),
+                torch.tensor(self.scaled_anchors[0]),
             )
             + self.loss_fn(
                 outputs[1].to(config.DEVICE),
                 labels[1].to(config.DEVICE),
-                torch.tensor(config.ANCHORS[1]).to(config.DEVICE),
+                torch.tensor(self.scaled_anchors[1]),
             )
             + self.loss_fn(
                 outputs[2].to(config.DEVICE),
                 labels[2].to(config.DEVICE),
-                torch.tensor(config.ANCHORS[2]).to(config.DEVICE),
+                torch.tensor(self.scaled_anchors[2]),
             )
         )
 
@@ -110,13 +112,30 @@ if __name__ == "__main__":
     loss_fn = YoloV3Loss().to(config.DEVICE)
     model = YoloV3(20).to(config.DEVICE)
     optimizer = torch.optim.Adam
-    epochs = 100
+    epochs = 500
     lr = 0.001
     batch_size = 4
     checkpoint_path = config.CHECKPOINT_PATH
+    scaled_anchors = (
+        torch.tensor(config.ANCHORS)
+        * torch.tensor(
+            [config.IMAGE_SIZE // 32, config.IMAGE_SIZE // 16, config.IMAGE_SIZE // 8]
+        )
+        .unsqueeze(1)
+        .unsqueeze(1)
+        .repeat(1, 3, 2)
+    ).to(config.DEVICE)
 
     trainer = Trainer(
-        model, loader, loss_fn, optimizer, epochs, lr, batch_size, checkpoint_path
+        model,
+        loader,
+        loss_fn,
+        scaled_anchors,
+        optimizer,
+        epochs,
+        lr,
+        batch_size,
+        checkpoint_path,
     )
     # trainer.train()
     trainer.load_checkpoint(checkpoint_path)
@@ -124,5 +143,5 @@ if __name__ == "__main__":
 
     model.eval()
     predictions = model(images.to(config.DEVICE))
-    check_accuracy(predictions, labels)
-    # plot_prediction(images, predictions, 0)
+    #    check_accuracy(predictions, labels)
+    plot_prediction(images, predictions, 0)
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from tkinter import W
 import torch
 from matplotlib import axes, patches
 from matplotlib import pyplot as plt
@@ -42,11 +43,11 @@ def intersection_over_union(
 
 
 def cell_to_image_coords(
-    cell_size: int, cell_coord: torch.Tensor, box_coord: torch.Tensor
+    cell_size: int, cell_coord: torch.Tensor, x_y: torch.Tensor, w_h: torch.Tensor
 ) -> torch.Tensor:
     cell_coord = cell_coord * (config.IMAGE_SIZE / cell_size)
-    x_y = (box_coord[0:2] * cell_size + cell_coord) / config.IMAGE_SIZE
-    w_h = box_coord[2:4] / cell_size
+    x_y = (x_y * (config.IMAGE_SIZE / cell_size) + cell_coord) / config.IMAGE_SIZE
+    w_h = w_h / cell_size
     return torch.cat([x_y, w_h])
 
 
@@ -56,40 +57,61 @@ def plot_prediction(image, predictions: torch.Tensor, pred_no: int):
     boxes_by_class = {}
     for scale in range(3):
 
-        labels_obj = predictions[scale][pred_no][..., 0] > 0.6
-        obj_preds = predictions[scale][pred_no][..., 0:][labels_obj]
-        obj_preds_indices = labels_obj.nonzero()
+        obj_indices = torch.sigmoid(predictions[scale][pred_no][..., 0]) > 0.8
+        obj_preds = predictions[scale][pred_no][..., 0:][obj_indices]
+        obj_cell_indices = obj_indices.nonzero()
         for i, obj_pred in enumerate(obj_preds):
             obj_class = torch.argmax(obj_pred[5:]).item()
-            # obj_class = obj_pred[5].item()
-            obj_coords = cell_to_image_coords(
-                config.CELL_SIZES[scale], obj_preds_indices[i][2:4], obj_pred[1:5]
+            x_y = torch.sigmoid(obj_pred[1:3])
+            anchors = (
+                torch.tensor(config.ANCHORS[scale]).to(config.DEVICE)
+                * config.CELL_SIZES[scale]
             )
-            obj_with_score = [obj_pred[0].item(), *obj_coords.tolist()]
+            w_hs = torch.exp(obj_pred[3:5]) * anchors
+            objs_coords = [
+                cell_to_image_coords(
+                    config.CELL_SIZES[scale], obj_cell_indices[i][1:3], x_y, w_h
+                )
+                for w_h in w_hs
+            ]
+            objs_with_score = [
+                [torch.sigmoid(obj_pred[0]).item(), *obj_coords.tolist()]
+                for obj_coords in objs_coords
+            ]
             if obj_class not in boxes_by_class:
-                boxes_by_class[obj_class] = [obj_with_score]
+                boxes_by_class[obj_class] = objs_with_score
             else:
-                boxes_by_class[obj_class] += [obj_with_score]
+                boxes_by_class[obj_class] += objs_with_score
 
-    supressed_boxes = nms(
-        center_to_edge_coords(torch.tensor(boxes_by_class[12])[..., 1:5]),
-        torch.tensor(boxes_by_class[12])[..., 0],
-        iou_threshold=0.2,
-    )
-    for box in torch.tensor(boxes_by_class[12])[supressed_boxes]:
-        draw_box(box[1:5], ax, "r", image.shape[2], image.shape[3])
+    for class_no in boxes_by_class.keys():
+        supressed_boxes = nms(
+            center_to_edge_coords(torch.tensor(boxes_by_class[class_no])[..., 1:5]),
+            torch.tensor(boxes_by_class[class_no])[..., 0],
+            iou_threshold=0.35,
+        )
+        for box in torch.tensor(boxes_by_class[class_no])[supressed_boxes]:
+            draw_box(box[1:5], ax, "r", image.shape[2], image.shape[3])
 
     plt.show()
 
 
 def check_accuracy(predictions: torch.Tensor, labels: torch.Tensor):
     for scale in range(3):
+        anchors = (
+            torch.tensor(config.ANCHORS[scale]).to(config.DEVICE)
+            * config.CELL_SIZES[scale]
+        )
         labels_obj = labels[scale][0][..., 0] == 1
         label_data = labels[scale][0][..., 0:6][labels_obj]
-        pred_data = predictions[scale][0][..., 0:5][labels_obj]
+        pred_boxes = predictions[scale][0][..., 1:5][labels_obj]
         pred_classes = [
             torch.argmax(args) for args in predictions[scale][0][..., 5:][labels_obj]
         ]
+        pred_boxes[..., 0:2] = torch.sigmoid(pred_boxes[..., 0:2])
+        # pred_boxes[..., 2:4] = torch.exp(pred_boxes[..., 2:4]) * anchors.reshape(
+        #     3, 1, 1, 2
+        # )
+        pred_boxes
 
 
 def center_to_edge_coords(boxes: torch.Tensor) -> torch.Tensor:
